cf0ffe1a11e5f8223157593bb96745be50932f06
Abdelmoula72 <abdelmoula.elamri@gmail.com>
2025-04-05 02:34:56 +0200

    import d'images correct pour les produits

--- app_yemconsulting/urls.py
+++ app_yemconsulting/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static  # ← celle-ci manque chez toi
+
 
 urlpatterns = [
     # Page d'accueil = grandes catégories uniquement
--- yemconsulting/urls.py
+++ yemconsulting/urls.py
@@ -14,11 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path, include  # Assurez-vous d'importer "include"
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app_yemconsulting.urls')),  # Inclure les URLs de l'application
-]
+    path('', include('app_yemconsulting.urls')),
+]  # 👈 bien fermer la liste ici avec le crochet
 
+# Ajouter les fichiers médias seulement si DEBUG est activé (optionnel mais propre)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
